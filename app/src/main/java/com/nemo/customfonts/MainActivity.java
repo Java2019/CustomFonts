071daab1837d48497170822a710a6bc328eb68b1
java2019 <java_2019@mail.ru>
2017-07-12 11:40:00 +0300

    first

--- app/src/main/java/com/nemo/customfonts/MainActivity.java
+++ app/src/main/java/com/nemo/customfonts/MainActivity.java
@@ -3,6 +3,7 @@ package com.nemo.customfonts;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         Typeface custom_font = Typeface.createFromAsset(getAssets(),
                 "font/Painter_PERSONAL_USE_ONLY.ttf");
         textView.setTypeface(custom_font);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
